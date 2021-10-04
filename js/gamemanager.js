d912da2fe65140823c5fddfc004ce63af0aa7b0c
Karin Kreeft <karinkreeft8@hotmail.com>
2021-10-04 15:04:23 +0200

    First person control

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -10,31 +10,38 @@ import {
     THREE,
     OrbitControls,
     GLTFLoader,
-    TransformControls
+    TransformControls,
+    FirstPersonControls,
+    PointerLockControls
 } from "../js/scene_setup";
 
 import {
     player
 } from "../js/player";
 
-let debug_mode = true;
-
 //Var declarations
-const orbitControls = new OrbitControls(camera, renderer.domElement);
+let debug_mode = true;
 const loader = new GLTFLoader();
+//const orbitControls = new OrbitControls(camera, renderer.domElement);
 const controls = new TransformControls(camera, renderer.domElement);
+const pointerControls = new PointerLockControls(camera, renderer.domElement);
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-let player_model;
-let player_height;
-let distance;
+let distance_ground = 1.6;
+let clock = new THREE.Clock();
+let movePlayer = true;
+const fpcontrols = new FirstPersonControls(camera, renderer.domElement);
+fpcontrols.movementSpeed = 0;
+fpcontrols.lookSpeed = 0;
+fpcontrols.lookVertical = true;
+controls.noFly = true;
 
 
 //debug mode
 if(debug_mode){
     let gridHelper = new THREE.GridHelper(10,10);
     const axesHelper = new THREE.AxesHelper( 5 );
-    orbitControls.update();
+    //orbitControls.update();
     scene.add(gridHelper, axesHelper);
     //When pressing G, R or S change the transform controls mode
     window.addEventListener('keydown', function (event) {
@@ -53,10 +60,10 @@ if(debug_mode){
 
     // When clicking disable orbitControls. You cant move object easily otherwise
     controls.addEventListener('mouseDown', function () {
-        orbitControls.enabled = false;
+       // orbitControls.enabled = false;
     });
     controls.addEventListener('mouseUp', function () {
-        orbitControls.enabled = true;
+        //orbitControls.enabled = true;
     });
 
     //When a click event is triggered get mouse location
@@ -67,12 +74,9 @@ if(debug_mode){
        // SelectModel();
     });
 }
-camera.position.z = 3;
-camera.position.y = 2;
-camera.rotation.x = -10*(Math.PI/180);
-orbitControls.enablePan = false;
-orbitControls.enableRotate = false;
-orbitControls.enableZoom = false;
+camera.position.z = 0;
+camera.position.y = distance_ground;
+
 
 //Select the model that is clicked (has a raycast hit)
 function SelectModel(){
@@ -117,35 +121,25 @@ models.forEach(element => {
     }
 });
 
-//Load the player model
-loader.load(player.modelinfo.src, function(gltf){
-    //console.log(gltf);
-    player_model = gltf.scene;
-    //console.log(player_model);
-    player_model.position.set(player.modelinfo.x_pos, player.modelinfo.y_pos, player.modelinfo.z_pos);
-    player_model.rotation.set(player.modelinfo.x_rot*(Math.PI/180), player.modelinfo.y_rot*(Math.PI/180), player.modelinfo.z_rot*(Math.PI/180));
-    player_model.attach(camera);
-    player_model.attach(pointLight);
-    //Get the mesh from the object
-    player_model.traverse((o) => {
-        //Set the object material to the toonshader using the embedded texture
-        if(o.isMesh){
-            o.material = new THREE.MeshToonMaterial({map: o.material.map});
-            o.receiveShadow = true;
-            o.castShadow = true;
-            player_height = o.geometry.boundingBox.max.y;
-        }
-    });
-    scene.add(player_model);
-});
-
-window.addEventListener("click", function(e){
+//Doesnt work on mobile
+window.addEventListener("dblclick", function(e){
     mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
 
     //Execute MovePlayer
-    MovePlayer();
+    if(movePlayer == true){
+        MovePlayer();
+    }
 });
+window.addEventListener("mousedown", function(e){
+    fpcontrols.lookSpeed = 0.2;
+    movePlayer = false;
+});
+window.addEventListener("mouseup", function(e){
+    fpcontrols.lookSpeed = 0;
+    movePlayer = true;
+});
+
 //Move the player to the location that was clicked (has a raycast hit)
 function MovePlayer(){
     raycaster.setFromCamera(mouse, camera);
@@ -155,23 +149,20 @@ function MovePlayer(){
     clickLocation.forEach(element => {
         //Only if the hit object is a mesh look for the name of the object it is in
         if(element.object.type == "Mesh"){
-            //If the object is the ground. Get the x, y, z position
+        //If the object is the ground. Get the x, y, z position
             if(element.object.parent.name == "ground"){
-                distance = element.distance;
+                //distance = element.distance;
                 const x = element.point.x;
-                const y = element.point.y;
+                distance_ground = element.point.y + 1.6;
                 const z = element.point.z;
-                player_model.position.set(x, y, z);
-                //Lerp position. Use the distance gained and divide that by step/speed size until location is reached
-                //player_model.position.lerp(new THREE.Vector3(x,y,z), 0,5);
+                camera.position.set(x, distance_ground, z);
+                }
             }
-        }
     });
-
 }
 
 function RenderLoop() {
-    //var delta = clock.getDelta();
+    fpcontrols.update(clock.getDelta());
 	requestAnimationFrame(RenderLoop);
 	Render();
 }
